39762fec39348f8058865a1c1eaba9d137253cbe
Saein Pak <saeinpak@gmail.com>
2026-01-19 07:04:44 -0800

    Added memoite sword abilities and some lore

--- src/main/java/net/insanescanner/tutorialmod/entity/custom/MemoiteSwordProjectileEntity.java
+++ src/main/java/net/insanescanner/tutorialmod/entity/custom/MemoiteSwordProjectileEntity.java
@@ -2,6 +2,7 @@ package net.insanescanner.tutorialmod.entity.custom;
 import net.insanescanner.tutorialmod.entity.ModEntities;
 import net.insanescanner.tutorialmod.item.ModItems;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -18,58 +19,31 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 
 public class MemoiteSwordProjectileEntity extends AbstractArrow {
     private float rotation;
     public Vec2 groundedOffset;
-    private ItemStack thrownStack = ItemStack.EMPTY;
 
-
-    private static final EntityDataAccessor<ItemStack> DATA_ITEM_STACK =
-            SynchedEntityData.defineId(MemoiteSwordProjectileEntity.class, EntityDataSerializers.ITEM_STACK);
 
     public MemoiteSwordProjectileEntity(EntityType<? extends AbstractArrow> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
 
     }
 
-    public MemoiteSwordProjectileEntity(LivingEntity shooter, Level level,ItemStack itemStack) {
+    public MemoiteSwordProjectileEntity(LivingEntity shooter, Level level) {
         super(ModEntities.MEMOITE_SWORD_PROJECTILE_ENTITY.get(), shooter, level, new ItemStack(ModItems.MEMOITE_SWORD.get()), null);
 
-        this.entityData.set(DATA_ITEM_STACK, itemStack.copy());
+
 
     }
 
-    @Override
-    protected void defineSynchedData(SynchedEntityData.Builder pBuilder) {
-        super.defineSynchedData(pBuilder);
-        pBuilder.define(DATA_ITEM_STACK, new ItemStack(ModItems.MEMOITE_SWORD.get()));
-    }
 
-    @Override
-    public void addAdditionalSaveData(CompoundTag pCompound) {
-        super.addAdditionalSaveData(pCompound);
-        // FIX 3: Save the stack from the DataTracker, not the field
-        ItemStack stack = this.entityData.get(DATA_ITEM_STACK);
-        if (!stack.isEmpty()) {
-            pCompound.put("ThrownStack", stack.save(this.level().registryAccess(), new CompoundTag()));
-        }
-    }
-
-    @Override
-    public void readAdditionalSaveData(CompoundTag pCompound) {
-        super.readAdditionalSaveData(pCompound);
-        if (pCompound.contains("ThrownStack", 10)) {
-            ItemStack stack = ItemStack.parse(this.level().registryAccess(), pCompound.getCompound("ThrownStack")).orElse(ItemStack.EMPTY);
-            // FIX 4: Update the DataTracker when loading from disk
-            this.entityData.set(DATA_ITEM_STACK, stack);
-        }
-    }
 
     @Override
     protected ItemStack getDefaultPickupItem() {
         // This is now the source of truth
-        return this.entityData.get(DATA_ITEM_STACK);
+        return null;
     }
 
 
@@ -93,7 +67,7 @@ public class MemoiteSwordProjectileEntity extends AbstractArrow {
 
         if (!this.level().isClientSide) {
             this.level().broadcastEntityEvent(this, (byte)3);
-            //this.discard();
+            this.discard();
         }
     }
 
@@ -101,12 +75,10 @@ public class MemoiteSwordProjectileEntity extends AbstractArrow {
     protected void onHitBlock(BlockHitResult result) {
 
 
-        super.onHitBlock(result);
-
         if(result.getDirection() == Direction.SOUTH) {
             groundedOffset = new Vec2(215f,180f);
         }
-        if(result.getDirection() == Direction.NORTH) {
+        /*if(result.getDirection() == Direction.NORTH) {
             groundedOffset = new Vec2(215f, 0f);
         }
         if(result.getDirection() == Direction.EAST) {
@@ -121,6 +93,8 @@ public class MemoiteSwordProjectileEntity extends AbstractArrow {
         }
         if(result.getDirection() == Direction.UP) {
             groundedOffset = new Vec2(285f,180f);
-        }
+        }*/
+
+        this.discard();
     }
 }
--- src/main/java/net/insanescanner/tutorialmod/item/custom/memoite_tools/MemoiteSwordItem.java
+++ src/main/java/net/insanescanner/tutorialmod/item/custom/memoite_tools/MemoiteSwordItem.java
@@ -1,6 +1,8 @@
 package net.insanescanner.tutorialmod.item.custom.memoite_tools;
 
+import net.insanescanner.tutorialmod.component.ModDataComponentTypes;
 import net.insanescanner.tutorialmod.entity.custom.MemoiteSwordProjectileEntity;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -10,28 +12,39 @@ import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraftforge.event.level.BlockEvent;
 
 import java.util.List;
 
-public class MemoiteSwordItem extends SwordItem {
+public class MemoiteSwordItem extends SwordItem { ;
 
 
     public MemoiteSwordItem(Tier pTier, Properties pProperties) {
+
         super(pTier, pProperties);
+
     }
 
     @Override
     public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
 
+        if(Screen.hasShiftDown()){
+            pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.memoite_sword.shifted"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.memoite_sword.non_shifted"));
+        }
         super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
-/*
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
@@ -45,9 +58,7 @@ public class MemoiteSwordItem extends SwordItem {
                 return new InteractionResultHolder<>(InteractionResult.FAIL, itemstack);
             }
             System.out.println("CREATING ENTITY");
-            itemstack.hurtAndBreak(10, (ServerLevel) pLevel, (ServerPlayer) pPlayer, item -> {
-            });
-            MemoiteSwordProjectileEntity MemoiteSwordProjectile = new MemoiteSwordProjectileEntity((LivingEntity) pPlayer, pLevel, itemstack.copy());
+            MemoiteSwordProjectileEntity MemoiteSwordProjectile = new MemoiteSwordProjectileEntity((LivingEntity) pPlayer, pLevel);
             System.out.println("CREATING ENTITY2");
             MemoiteSwordProjectile.shootFromRotation(pPlayer, pPlayer.getXRot(), pPlayer.getYRot(), 0.0F, 1.5F, 0F);
 
@@ -57,15 +68,25 @@ public class MemoiteSwordItem extends SwordItem {
 
         pPlayer.awardStat(Stats.ITEM_USED.get(this));
         if (!pPlayer.getAbilities().instabuild) {
-            itemstack.shrink(1);
+
+            pPlayer.hurt(pPlayer.damageSources().thrown(pPlayer, pPlayer), 5);
+            itemstack.hurtAndBreak(10, pPlayer, EquipmentSlot.MAINHAND);
+            pPlayer.getCooldowns().addCooldown(this, 300);
         }
 
         return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
     }
-*/
+
 
     private static boolean playerHasShieldUseIntent(UseOnContext pContext) {
         Player player = pContext.getPlayer();
         return pContext.getHand().equals(InteractionHand.MAIN_HAND) && player.getOffhandItem().is(Items.SHIELD) && !player.isSecondaryUseActive();
     }
+
+    @Override
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+
+        super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
+    }
+
 }
